88087ef655ee66286a09d5c1777458abfaf392de
Ben Carter <benfcarter@gmail.com>
2025-03-29 00:38:28 -0400

    Exercise 1.8 - Make Statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  let total = good + neutral + bad
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}<br />
+        all {total}<br />
+        average {(good - bad) / (total)}<br />
+        positive {(100 * good) / (total)}%
+      </p>
+    </div>
+)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -12,15 +30,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {good + neutral + bad}<br />
-        average {(good - bad) / (good + neutral + bad)}<br />
-        positive {(100 *good) / (good + neutral + bad)}%
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
