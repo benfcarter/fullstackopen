1c2ce1a0ed39be0e9d7f62c545f49e473dadf94c
Ben Carter <benfcarter@gmail.com>
2025-03-30 01:21:41 -0400

    Exercise 2.5 - Move Course into its own module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,4 @@
-import { useState } from 'react'
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    course.parts.map((part) => <Part key={part.id} part={part} />)
-  )
-}
-
-const ExerciseSum = ({ course }) => {
-  let sum = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <p>
-      <strong>
-        total of {sum} exercises
-      </strong>
-    </p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <ExerciseSum course={course} />
-    </>
-  )
-}
+import Course from './Course'
 
 const CourseListHeader = ({ name }) => {
   return (
