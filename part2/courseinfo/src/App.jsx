ab9b9a03311e9a22d3b5c112a886560ecfb7ac22
Ben Carter <benfcarter@gmail.com>
2025-03-30 01:19:30 -0400

    Exercise 2.4 - Support for multiple courses

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
   )
 }
 
@@ -40,35 +40,67 @@ const Course = ({ course }) => {
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      },
-    ]
-  }
+const CourseListHeader = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
 
-  return <Course course={course} />
+const CourseList = ({ courseList }) => {
+  return (
+    <>
+      <CourseListHeader name={"Web development curriculum"} />
+      {courseList.map((course) => <Course course={course} key={course.id} />)}
+    </>
+  )
+}
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return <CourseList courseList={courses} />
 }
 
 export default App
