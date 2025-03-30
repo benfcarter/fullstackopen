228b82d3b4c57972984c4d88259d61774637f163
Ben Carter <benfcarter@gmail.com>
2025-03-30 01:54:20 -0400

    Exercise 2.6 - Add numbers to phonebook using form

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,6 +14,7 @@ const CourseList = ({ courseList }) => {
     </>
   )
 }
+
 const App = () => {
   const courses = [
     {
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const NumberList = ({ numbers }) => {
+  return (
+    <>
+      {numbers.map((x) => <div key={x.name}>{x.name}</div>)}
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+  }
+
+  console.log(persons);
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <NumberList numbers={persons} />
+    </div>
+  )
+}
+
+export default App
