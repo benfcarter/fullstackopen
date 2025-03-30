3fa83b3751531ed8e555e6852440236f62c8d763
Ben Carter <benfcarter@gmail.com>
2025-03-30 02:02:43 -0400

    Exercise 2.7 - Don't allow duplicate names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -20,7 +20,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName}))
+
+    if(persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already in the phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName}))
+    }
   }
 
   console.log(persons);
