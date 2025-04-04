af5bee3886755ecf4492a120c0605178fce67580
Ben Carter <benfcarter@gmail.com>
2025-04-03 23:44:06 -0400

    Exercise 2.18 - Data for countries part 1

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -1,11 +1,98 @@
 import { useState, useEffect } from 'react'
+import countryService from './services/countries'
+
+const SearchBox = ({ value, onChange }) => {
+  return (
+    <>
+      find countries <input value={value} onChange={onChange} />
+    </>
+  )
+}
+
+const CountryInfoEntry = ({ match }) => {
+  return (
+    <div>{match.name.common}</div>
+  )
+}
+
+const SingleVerboseCountryInfo = ({ match }) => {
+  return (
+    <>
+      <h1>{match.name.common}</h1>
+      <div>Capital {match.capital}</div>
+      <div>Area {match.area}</div>
+      <h2>Languages</h2>
+      <ul >
+        {Object.values(match.languages).map(x => <li key={x}>{x}</li>)}
+      </ul>
+      <img src={match.flags.png} alt={match.flags.alt} />
+    </>
+  )
+}
+
+const CountryInfo = ({ matches }) => {
+  if(matches.length > 10) {
+    return (
+      <>
+        Too many matches, specify another filter
+      </>
+    )
+  } else if(matches.length === 0) {
+    return (
+      <>
+        No countries match
+      </>
+    )
+  } else if(matches.length > 1) {
+    return (
+      <>
+        {matches.map(match => <CountryInfoEntry key={match.ccn3} match={match} />)}
+      </>
+    )
+  } else {
+    return (
+      <>
+        <SingleVerboseCountryInfo match={matches[0]} />
+      </>
+    )
+  }
+}
 
 const App = () => {
- 
+  const [countryFilter, setCountryFilter] = useState('')
+  const [countryData, setCountryData] = useState(null)
+
+  const onCountryFilterChange = (event) => {
+    setCountryFilter(event.target.value)
+  }
+
+  const getMatches = () => {
+    if(countryData === null) {
+      return []
+    }
+
+    if(countryFilter === '') {
+      return []
+    }
+
+    console.log(countryFilter)
+
+    return countryData.filter(x => x.name.common.toUpperCase().includes(countryFilter.toUpperCase()))
+  }
+
+  console.log(`Current country filter: ${countryFilter}`)
+
+  useEffect(() => {
+    console.log(`useEffect`)
+    countryService.getAll()
+    .then(data => setCountryData(data))
+  }, [])
+
   return (
-    <div>
-      Hello world!
-    </div>
+    <>
+      <div><SearchBox value={countryFilter} onChange={onCountryFilterChange} /></div>
+      <div><CountryInfo matches={getMatches()} /></div>
+    </>
   )
 }
 
