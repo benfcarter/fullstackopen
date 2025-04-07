fe5aa706e491acaee7ca77fcd571b617135d00ed
Ben Carter <benfcarter@gmail.com>
2025-04-07 01:24:09 -0400

    Exercise 3.22 - Fix some line warnings in frontend (it got stuc on React code

--- part3/phonebook-fe/src/services/persons.js
+++ part3/phonebook-fe/src/services/persons.js
@@ -3,19 +3,19 @@ import axios from 'axios'
 const baseUrl = '/api/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl).then(response => response.data)
+  return axios.get(baseUrl).then(response => response.data)
 }
 
 const create = (newObject) => {
-    return axios.post(baseUrl, newObject).then(response => response.data)
+  return axios.post(baseUrl, newObject).then(response => response.data)
 }
 
 const remove = (id) => {
-    return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
 }
 
 const update = (changedObject) => {
-    return axios.put(`${baseUrl}/${changedObject.id}`, changedObject).then(response => response.data)
+  return axios.put(`${baseUrl}/${changedObject.id}`, changedObject).then(response => response.data)
 }
 
 export default { getAll, create, remove, update }
