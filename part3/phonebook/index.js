59fe66176f4679b77baac830115ffeb07ef0f24c
Ben Carter <benfcarter@gmail.com>
2025-04-06 23:57:08 -0400

    Exercise 3.16 - Hook up error handling middleware

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -43,49 +43,46 @@ let persons = [
     }
 ]
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({})
         .then(result => {
             response.json(result)
         })
-        .catch(error => {
-            console.log(`Error fetching person data: ${error.message}`)
-            response.status(404).end()
-        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id.toString())
+app.get('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id.toString()
+    Person.findById(id)
         .then(person => {
-            response.json(person)
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).json({ error: `Could not find person with id ${id}`})
+            }
         })
-        .catch(error => {
-            response.status(404).json({ error: `Could not find person with id ${request.params.id}`})
-        })
+        .catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
+app.get('/api/info', (request, response, next) => {
     Person.countDocuments()
         .then(result => {
-            console.log(result)
             response.send(`<p>Phonebook has info for ${result} ${result === 1 ? "person" : "people"}</p><p>${Date()}</p>`)
         })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id.toString())
         .then(result => {
             response.status(204).end()
         })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if(!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'Phonebook entries must contain both a "name" and "number"'
-        })
+        next(error)
     }
 
     const person = new Person({
@@ -97,12 +94,20 @@ app.post('/api/persons', (request, response) => {
         .then(savedPerson => {
             response.json(savedPerson)
         })
-        .catch(error => {
-            response.status(409).json({
-                error: `Error adding person to DB: ${error.message}`
-            })
-        })
+        .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error: `Malformatted Id`})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
