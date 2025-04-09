048ced98d2cc1aa7a87b37e0fde4b7711f7c7601
Ben Carter <benfcarter@gmail.com>
2025-04-09 13:36:14 -0400

    Exercise 4.2 - Refactor bloglist into separate modules

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,38 +1,7 @@
-const config =  require('./utils/config')
+const app = require('./app')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const mongoose = require('mongoose')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const port = config.PORT
-app.listen(port, () => {
-  logger.info(`Server running on port ${port}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
