644acc88bfe0bc21c8606aaae4d93d9940c30bf6
Ben Carter <benfcarter@gmail.com>
2025-04-12 19:37:52 -0400

    Exercise 4.10 - Add test for adding new blogs and refactor POST handler to use async/await

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,10 +1,59 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('node:assert')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const testBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  }  
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  
+  for(const blog of testBlogs) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -13,9 +62,9 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there is one blog', async () => {
+test('there are the correct number of blogs', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 1)
+  assert.strictEqual(response.body.length, testBlogs.length)
 })
 
 test('blog entries do not have _id', async () => {
@@ -28,6 +77,27 @@ test('blog entries have id', async () => {
   const response = await api.get('/api/blogs')
   const blog = response.body[0]
   assert(blog.hasOwnProperty('id'))
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Test blog",
+    author: "Ben Carter",
+    url: "https://google.com",
+    likes: 200
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(blog => blog.title)
+
+  assert.strictEqual(titles.length, testBlogs.length + 1)
+  assert(titles.includes(newBlog.title))
 })
 
 after(async () => {
