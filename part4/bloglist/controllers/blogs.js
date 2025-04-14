469ff1c80cb3b6dbcf26a79ba11a48b472ee0725
Ben Carter <benfcarter@gmail.com>
2025-04-13 22:45:28 -0400

    Exercise 4.21 - Only user that created a blog post can delete it

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -38,7 +38,24 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id.toString())
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id.toString())
+
+  if(!blog) {
+    response.status(204).end()  
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  if(blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'only the user that submitted the blog may delete it' })
+  }
+
+  await Blog.findOneAndDelete({ id: request.params.id })
   response.status(204).end()
 })
 
