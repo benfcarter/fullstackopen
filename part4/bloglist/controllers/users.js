e42d859421948c7abb332d16b4dccb539a07ed6c
Ben Carter <benfcarter@gmail.com>
2025-04-13 20:30:07 -0400

    Exercise 4.16 - Ensure usernames and passwords exist and are at least 3 characters long

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,7 +5,22 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, password, name } = request.body
 
-  console.log(username)
+  if(!username) {
+    return response.status(400).json('Missing username')
+  }
+
+  if(username.length < 3) {
+    return response.status(400).json('Username must be at least 3 characters long')
+  }
+
+  if(!password) {
+    return response.status(400).json('Missing password')
+  }
+
+  if(password.length < 3) {
+    return response.status(400).json('Password must be at least 3 characters long')
+  }
+
   const saltRounds = 10
   
   const salt = await bcrypt.genSalt(saltRounds)
--- part4/bloglist/tests/user_api.test.js
+++ part4/bloglist/tests/user_api.test.js
@@ -34,15 +34,92 @@ describe('when there are some users saved already', () => {
       .expect(201)
       .expect('Content-type', /application\/json/)
 
-    console.log(usersAtStart)
     const usersAtEnd = await User.find({})
-    console.log(usersAtEnd)
     assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
 
     const usernames = usersAtEnd.map(user => user.username)
     assert(usernames.includes('bigbc79'))
   })
-})
+
+  test('creation fails with a missing username', async () => {
+    const usersAtStart = await User.find({})
+
+    const newUser = {
+      name: 'Ben Carter',
+      password: 'TheBestPassword',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAtEnd = await User.find({})
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+
+    assert(response.body.includes('Missing username'))
+  })
+    
+  test('creation fails with a username that is too short', async () => {
+    const usersAtStart = await User.find({})
+
+    const newUser = {
+      username: 'x',
+      name: 'Ben Carter',
+      password: 'TheBestPassword',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAtEnd = await User.find({})
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+
+    assert(response.body.includes('must be at least'))
+  })
+
+  test('creation fails with a missing password', async () => {
+    const usersAtStart = await User.find({})
+
+    const newUser = {
+      username: 'bigbc79',
+      name: 'Ben Carter',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAtEnd = await User.find({})
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+
+    assert(response.body.includes('Missing password'))
+  })
+    
+  test('creation fails with a password that is too short', async () => {
+    const usersAtStart = await User.find({})
+
+    const newUser = {
+      username: 'bigbc79',
+      name: 'Ben Carter',
+      password: 'a',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAtEnd = await User.find({})
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+
+    assert(response.body.includes('must be at least'))
+  })})
 
 after(async () => {
   await mongoose.connection.close()
