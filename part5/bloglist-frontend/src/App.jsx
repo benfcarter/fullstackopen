768e96468da4564ed8987c1d9cfa4620d98d9ad5
Ben Carter <benfcarter@gmail.com>
2025-04-17 21:21:25 -0400

    Exercise 5.1 - Implement basic login functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,63 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const Notification = ({ notification }) => {
+  if(notification === null) {
+    return null
+  }
+
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+
+  const successStyle = { ...errorStyle, color: 'green' }
+
+  return (
+    <div style={notification.isError ? errorStyle : successStyle}>
+      {notification.message}
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+
+      console.log(user)
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage({
+        message: 'Wrong credentials',
+        isError: true,
+      })
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +65,31 @@ const App = () => {
     )  
   }, [])
 
+  if(user === null) {
+    return (
+      <div>
+        <Notification notification={errorMessage} />
+        <h2>log in to application</h2>
+
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            password
+            <input type="password" value={password} name="Password" onChange = {({ target }) => setPassword(target.value)} />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>Logged in as {user.username}</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
