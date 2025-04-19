28e88733156a9b8e1da1d2166e3f8b11ac6f2fc5
Ben Carter <benfcarter@gmail.com>
2025-04-18 22:37:06 -0400

    Exercise 5.7 - Add show/hide button for the details of each blog

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showDetailsStyle = { display: showDetails ? '' : 'none' }
+  const hideDetailsStyle = { display: showDetails ? 'none' : '' }
+
+  console.log(blog)
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleDetails}>{showDetails ? 'hide' : 'show'}</button>
+      <div style={showDetailsStyle}>
+        <div>{blog.url}</div>
+        <div>{blog.likes}</div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
