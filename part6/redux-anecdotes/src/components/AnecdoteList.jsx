912afce73133a7ef9a1838f640be7088b718558d
Ben Carter <benfcarter@gmail.com>
2025-05-04 15:48:32 -0400

    Exercise 6.8 - Create AnecdoteList component

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(addVote(id))
+  }
+
+  return (
+    <>
+      {anecdotes.sort((a, b) => {
+        return a.votes > b.votes ? -1 : 1
+      }).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>    
+  )
+}
+
+export default AnecdoteList
