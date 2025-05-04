c7db91bae7c98ed7d265b24c22bc89c6dcc4b8f9
Ben Carter <benfcarter@gmail.com>
2025-05-04 15:00:51 -0400

    Exercise 6.1 - Implement basic actions tests for unicafe-redux

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,6 +1,15 @@
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
+const performAction = (state, actionName) => {
+  const action = {
+    type: actionName
+  }
+
+  deepFreeze(state)
+  return counterReducer(state, action)
+}
+
 describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
@@ -19,15 +28,37 @@ describe('unicafe reducer', () => {
   })
 
   test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
-    const state = initialState
-
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
+    const newState = performAction(initialState, 'GOOD')
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('okay is incremented', () => {
+    const newState = performAction(initialState, 'OK')
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const newState = performAction(initialState, 'BAD')
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('state is reset', () => {
+    const goodState = performAction(initialState, 'GOOD')
+    const newState = performAction(goodState, 'ZERO')
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
