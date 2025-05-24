f79a5592b1fba47ae3c95aa85ba5e685aade05f6
Ben Carter <benfcarter@gmail.com>
2025-05-23 22:18:01 -0400

    Exercise 8.4 - Add optional author parameter to allBooks query

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -113,7 +113,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -122,7 +122,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(b => !args.author || b.author === args.author),
     allAuthors: () => authors.map(x => ({
       name: x.name,
       bookCount: books.filter(b => b.author === x.name).length
