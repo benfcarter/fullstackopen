37e604d95f7d47bf1c19ce436e6dcc3c27692a4b
Ben Carter <benfcarter@gmail.com>
2025-05-24 23:02:20 -0400

    Exercise 8.10 - Implement CREATE_BOOK mutation to add new books to the list

--- part8/library/frontend/src/components/NewBook.jsx
+++ part8/library/frontend/src/components/NewBook.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { CREATE_BOOK } from '../queries/mutations'
+import { ALL_AUTHORS, ALL_BOOKS } from '../queries/queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,6 +11,10 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_AUTHORS }, { query: ALL_BOOKS }]
+  })
+
   if (!props.show) {
     return null
   }
@@ -14,7 +22,7 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    console.log('add book...')
+    createBook({ variables: { title, author, published: parseInt(published), genres }})
 
     setTitle('')
     setPublished('')
